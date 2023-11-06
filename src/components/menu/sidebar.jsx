c7cbed4c9684c7bf301fc26b617873ff190aa265
Noe <noe91.carranza@gmail.com>
2023-11-06 15:45:28 -0300

    funcion navbar

--- src/components/menu/sidebar.jsx
+++ src/components/menu/sidebar.jsx
@@ -1,102 +1,101 @@
-import React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
-import Face2Icon from '@mui/icons-material/Face2';
-import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import Face2Icon from "@mui/icons-material/Face2";
+import FingerprintIcon from "@mui/icons-material/Fingerprint";
 import logo from "../../assets/sonrisa4.png";
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
-import AppShortcutIcon from '@mui/icons-material/AppShortcut';
-import './menu.css';
-import { Link } from 'react-router-dom';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import { useMediaQuery } from '@mui/material';
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import AppShortcutIcon from "@mui/icons-material/AppShortcut";
+import "./menu.css";
+import { Link } from "react-router-dom";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import { useMediaQuery } from "@mui/material";
 import "../menu/menu.css";
-
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
-);
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 const ListaItems = [
-  { text: 'Quienes somos', path: '/quienes-somos' },
-  { text: 'Cómo funciona', path: '/como-funciona' },
-  { text: 'Quiero formar parte', path: '/postularse' },
-  { text: 'Tarifas', path: '/tarifas' },
-  { text: 'App', path: '/aplicacion' }
+  { text: "Quienes somos", path: "/quienes-somos" },
+  { text: "Cómo funciona", path: "/como-funciona" },
+  { text: "Quiero formar parte", path: "/postularse" },
+  { text: "Tarifas", path: "/tarifas" },
+  { text: "App", path: "/aplicacion" },
 ];
 
 const iconList = [
@@ -104,7 +103,7 @@ const iconList = [
   <PhoneAndroidIcon />,
   <Face2Icon />,
   <MonetizationOnIcon />,
-  <AppShortcutIcon />
+  <AppShortcutIcon />,
 ];
 
 function MiniDrawer() {
@@ -118,11 +117,16 @@ function MiniDrawer() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
+  const handleMenuClick = () => {
+    window.scrollTo(0, 0);
+  };
+  const handleLogoClick = () => {
+    window.scrollTo(0, 0);
+  };
   return (
     <div className="menu">
-      <AppBar open={open} className='appbar-menu' sx={{color:'black'}}>
-        <Toolbar className='menu-toolbar'>
+      <AppBar open={open} className="appbar-menu" sx={{ color: "black" }}>
+        <Toolbar className="menu-toolbar">
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -130,55 +134,70 @@ function MiniDrawer() {
             edge="start"
             sx={{
               marginRight: 5,
-              ...(open && { display: 'none' }),
+              ...(open && { display: "none" }),
             }}
           >
             <MenuIcon />
           </IconButton>
           <Link to="/">
-          <img src={logo} className='logo'></img>
+            <img
+              src={logo}
+              className="logo"
+              alt="logo app"
+              onClick={handleLogoClick}
+            ></img>
           </Link>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open} sx={{width:'50%'}}>
+      <Drawer variant="permanent" open={open} sx={{ width: "50%" }}>
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <List sx={{ display: 'block', padding:'0'}}>
-  {ListaItems.map((item, index) => (
-    <ListItem key={item.text} disablePadding>
-      <Link to={item.path} style={{ textDecoration: 'none', color: 'inherit'}}>
-        <ListItemButton
-          sx={{
-            minHeight: 100,
-            justifyContent: open ? 'initial' : 'center',
-            textAlign:'start',
-            px: 2.5,
-            width:'250px'
-
-          }}
-        >
-          <ListItemIcon
-            sx={{
-              minWidth: 0,
-              mr: open ? 3 : 'auto',
-              justifyContent: 'center',
-            }}
-          >
-            {iconList[index]}
-          </ListItemIcon>
-          <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
-        </ListItemButton>
-      </Link>
-    </ListItem>
-  ))}
-</List>
+        <List sx={{ display: "block", padding: "0" }}>
+          {ListaItems.map((item, index) => (
+            <ListItem key={item.text} disablePadding>
+              <Link
+                to={item.path}
+                style={{ textDecoration: "none", color: "inherit" }}
+                onClick={handleMenuClick}
+              >
+                <ListItemButton
+                  sx={{
+                    minHeight: 100,
+                    justifyContent: open ? "initial" : "center",
+                    textAlign: "start",
+                    px: 2.5,
+                    width: "250px",
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {iconList[index]}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item.text}
+                    sx={{ opacity: open ? 1 : 0 }}
+                  />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
     </div>
   );
 }
 
-export default MiniDrawer
+export default MiniDrawer;
