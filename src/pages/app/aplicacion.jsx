4e0c043dc436b430f0883db36dfe5dfd2013ab48
Noe <noe91.carranza@gmail.com>
2023-11-06 16:26:42 -0300

    .

--- src/pages/app/aplicacion.jsx
+++ src/pages/app/aplicacion.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../app/aplicacion.css'
-import { styled } from '@mui/material/styles';
+import { Link } from "react-router-dom";
 import AppleIcon from '@mui/icons-material/Apple';
 import Button from '@mui/material/Button';
 import ShopTwoIcon from '@mui/icons-material/ShopTwo';
@@ -16,6 +16,7 @@ function Aplicacion() {
                     <h2 className="subtitulo-app" style={{ paddingLeft: '20px', margin: '0' }}>Encontrá rápidamente una babysitter disponible las 24 horas los 7 días de la semana a través de nuestra app.</h2>
                 </div>
                 <div className="botones-store">
+                    <Link to='https://www.figma.com/proto/YM6MYpfq8bK1aNDuGjWOxb/La-Tata-App?page-id=0%3A1&type=design&node-id=213-11792&viewport=3052%2C1532%2C0.15&t=QkjfvbU621jIzwoF-1&scaling=min-zoom&starting-point-node-id=213%3A11493&mode=design'>
                     <Button startIcon={<AppleIcon />}
                         sx={{
                             backgroundColor: '#E87D91',
@@ -39,6 +40,8 @@ function Aplicacion() {
                         }}>
                         Apple Store
                     </Button>
+                    </Link>
+                    <Link to='https://www.figma.com/proto/YM6MYpfq8bK1aNDuGjWOxb/La-Tata-App?page-id=0%3A1&type=design&node-id=213-11792&viewport=3052%2C1532%2C0.15&t=QkjfvbU621jIzwoF-1&scaling=min-zoom&starting-point-node-id=213%3A11493&mode=design'>
                     <Button startIcon={<ShopTwoIcon />}
                         sx={{
                             backgroundColor: '#E87D91',
@@ -62,6 +65,7 @@ function Aplicacion() {
                         }}>
                         Play Store
                     </Button>
+                    </Link>
                 </div>
             </div>
             <div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../../pages/home/home.css'
-import qr from '../../assets/qr.png'
-
+import qr from '../../assets/app-1.png';
+import { Link } from "react-router-dom";
 function Home() {
     return (
         <section>
@@ -17,7 +17,8 @@ function Home() {
             <div className="section_2">
                 <div className="section2_texto">
                 <h2 className="titulo" style={{margin:'0', color:'white',}}>Con la app todo es más fácil</h2>
-                <h2 className="slogan" style={{color:'white',}}>Escaneá el QR y descarga nuestra app acá</h2>
+                <h2 className="slogan" style={{color:'white',}}></h2>
+                <Link to='https://www.figma.com/proto/YM6MYpfq8bK1aNDuGjWOxb/La-Tata-App?page-id=0%3A1&type=design&node-id=213-11792&viewport=3052%2C1532%2C0.15&t=QkjfvbU621jIzwoF-1&scaling=min-zoom&starting-point-node-id=213%3A11493&mode=design' className="slogan" style={{color:'white',}}> Escaneá el QR y descarga nuestra app, o hacé click acá</Link>
                 </div>
                 <div className="contenedor_qr">
                     <img src={qr} alt="qr de nuestra app" className="qr"></img>
